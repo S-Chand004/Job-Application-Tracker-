4f4fe7ff2f49cadc3d27a590a01ce3f738007670
Snehashish Chand <162430375+S-Chand004@users.noreply.github.com>
2025-12-23 19:06:17 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,12 @@ def create_app():
 
     mysql.init_app(app)
 
+    from app.home.routes import home_bp
     from app.applications.routes import applications_bp
     from app.auth.routes import auth_bp
     from app.analytics.routes import analytics_bp
 
+    app.register_blueprint(home_bp)
     app.register_blueprint(applications_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(analytics_bp)
