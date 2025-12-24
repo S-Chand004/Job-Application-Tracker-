b5ec8d13b878db39fde8cbe35e82ea84b5169acb
Snehashish Chand <162430375+S-Chand004@users.noreply.github.com>
2025-12-24 14:05:33 +0530

    Update routes.py

--- app/analytics/routes.py
+++ app/analytics/routes.py
@@ -1,7 +1,23 @@
-from flask import Blueprint
+from flask import Blueprint, session, render_template, redirect, url_for
+from app.models.db import mysql
 
 analytics_bp = Blueprint('analytics', __name__)
 
 @analytics_bp.route('/analytics')
 def analytics():
-    return 'This is the analytics page'
+    if 'user_id' not in session:
+        return redirect(url_for('auth.login'))
+    
+    user_id = session['user_id']
+
+    cur = mysql.connection.cursor()
+
+    cur.execute("SELECT status, COUNT(*) FROM applications WHERE user_id = %s GROUP BY status", (user_id,))
+    status_count = cur.fetchall()
+
+    cur.execute("SELECT applied_date, COUNT(*) FROM applications WHERE user_id = %s GROUP BY applied_date", (user_id,))
+    month_count = cur.fetchall()
+
+    cur.close()
+
+    return render_template('analytics.html', status_counts = status_count, month_counts = month_count)
