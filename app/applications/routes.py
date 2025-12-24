9d70c2eeab3a3bcd839b33b4d1c88eee89927642
Snehashish Chand <162430375+S-Chand004@users.noreply.github.com>
2025-12-24 13:37:10 +0530

    Add edit application route and functionality

--- app/applications/routes.py
+++ app/applications/routes.py
@@ -41,10 +41,45 @@ def view_applications():
 
     user_id = session['user_id']
     cur = mysql.connection.cursor()
-    cur.execute("SELECT company, role, status, applied_date, notes FROM applications WHERE user_id = (%s) ORDER BY applied_date DESC",
+    cur.execute("SELECT company, role, status, applied_date, notes, id FROM applications WHERE user_id = (%s) ORDER BY applied_date DESC",
                 (user_id,))
     
     apps = cur.fetchall()
     cur.close()
 
     return render_template('applications.html', apps = apps)
+
+@applications_bp.route('/edit/<app_id>', methods = ['GET', 'POST'])
+def edit_applications(app_id):
+    if 'user_id' not in session:
+        redirect(url_for('auth.login'))
+    
+    user_id = session['user_id']
+
+    cur = mysql.connection.cursor()
+    
+    if request.method == 'GET':
+        cur.execute("SELECT company, role, status, applied_date, notes FROM applications WHERE id=%s AND user_id = %s", 
+                    (app_id, user_id))
+        app_data = cur.fetchone()
+        cur.close()
+
+        if not app_data:
+            return "Application not found."
+    
+        return render_template('edit_application.html', app = app_data, app_id = app_id)
+
+    if request.method == 'POST':
+        company = request.form['company']
+        role = request.form['role']
+        status = request.form['status']
+        applied_date = request.form['applied_date']
+        notes = request.form['notes']
+
+        cur.execute("UPDATE applications SET company = %s, role = %s, status = %s, applied_date = %s, notes = %s WHERE id = %s AND user_id = %s", (company, role, status, applied_date, notes, app_id, user_id))
+
+        mysql.connection.commit()
+        cur.close()
+
+        return redirect(url_for('applications.view_applications'))
+    
